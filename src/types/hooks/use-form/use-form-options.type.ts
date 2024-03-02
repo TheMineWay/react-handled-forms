25b45610ec5762ca32f8b2b8a8b55ac895881189
TheMineWay <joelcamposoliva9000@gmail.com>
2024-03-02 16:40:30 +0100

    Better type

--- src/types/hooks/use-form/use-form-options.type.ts
+++ src/types/hooks/use-form/use-form-options.type.ts
@@ -1,9 +1,13 @@
 import { ObjectSchema } from "yup";
 import { FormValuesModel } from "../../models/form-values-model.type";
 
+type MethodResult = void | undefined | never | unknown | any | null;
+
 export interface UseFormOptions<T extends FormValuesModel> {
   objectSchema?: ObjectSchema<T>;
-  onSubmit?: (values: T) => Promise<void>;
-  onSubmitError?: <T = unknown>(error: T) => void | Promise<void>;
+  onSubmit?: (values: T) => MethodResult | Promise<MethodResult>;
+  onSubmitError?: <T = unknown>(
+    error: T
+  ) => MethodResult | Promise<MethodResult>;
   defaultValues?: Partial<T>;
 }
