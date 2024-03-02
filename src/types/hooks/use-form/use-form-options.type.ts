6e774766ccf13b0e1ea3aed390a5d096859f6ecf
TheMineWay <joelcamposoliva9000@gmail.com>
2024-03-02 17:00:57 +0100

    Make global type

--- src/types/hooks/use-form/use-form-options.type.ts
+++ src/types/hooks/use-form/use-form-options.type.ts
@@ -3,9 +3,9 @@ import { FormValuesModel } from "../../models/form-values-model.type";
 
 type MethodResult = void | undefined | never | unknown | any | null;
 
-export interface UseFormOptions<T extends FormValuesModel> {
+export interface UseFormOptions<T extends FormValuesModel, E = any> {
   objectSchema?: ObjectSchema<T>;
   onSubmit?: (values: T) => MethodResult | Promise<MethodResult>;
-  onSubmitError?: <E>(error: E) => MethodResult | Promise<MethodResult>;
+  onSubmitError?: (error: E) => MethodResult | Promise<MethodResult>;
   defaultValues?: Partial<T>;
 }
